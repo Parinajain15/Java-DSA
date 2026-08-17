13577e7d3f921c62304039939b774ce7aae0cf1d
Parinajain15 <parinajainindore@gmail.com>
2026-08-17 23:53:09 +0530

    Add LC1342 Number of Steps to Reduce a Number to Zero

---
+++ 03_LeetCode/LC1342_NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,24 @@
+public class LC1342_NumberOfStepsToReduceANumberToZero
+{
+    public static void main(String args[])
+    {
+        int num = 14;
+        int steps = 0;
+
+        while(num > 0)
+        {
+            if(num % 2 == 0)
+            {
+                num = num / 2;
+            }
+            else
+            {
+                num = num - 1;
+            }
+
+            steps++;
+        }
+
+        System.out.println(steps);
+    }
+}
